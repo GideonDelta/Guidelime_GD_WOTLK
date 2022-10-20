5003a94809fd1067c9ea272eaa9387b3262cb040
GideonDelta <39777756+GideonDelta@users.noreply.github.com>
2022-10-20 03:21:40 -0400

    directory shuffle

---
+++ Achievements/GD_ToT_Outlands.lua
@@ -0,0 +1,69 @@
+-- Copyright © 2022 Matthew "GideonDelta" Edmund. All rights reserved.
+-- This add-on is free to download and use. That said, this add-on and its contents may not be redistributed, repackaged, modified, or transferred without explicit and detailed permission of the author.
+-- For questions and feedback childofnewlight@proton.me
+Guidelime.registerGuide(
+[[
+[GA Horde]
+[N Tricks and Treats of Outlands]
+[D Guide designed for efficient completion of the Tricks and Treats of Azeroth achievement for Horde characters:  https://Fyre.is/]
+[NX Tricks and Treats of Eastern Kingdoms]
+[NX Tricks and Treats of Kalimdor]
+Welcome to *GideonDelta's Guides to WOTLK:* Tricks and Treats of Outlands
+This guide is *intended* to be used by characters at the level of the highest zone entered with the apropriate flight paths and riding skill
+As flight is available in Outlands, Optional Flight Path instructions are marked with FP: \\ If you're using your own mount you can safely skip these
+My guides focus on efficiency. Whenever possible, I will take you the most direct route with the least amount of time between objectives and the least amount of backtracking.
+Go to the [G84.8,17.2,10 Undercity][C] Blasted Lands Portal in the Magic Quarter \\ We're picking up from the Eastern Kingdoms Guide. \\ If in another capital, skip this step and use the Blasted Lands Portal in that city.
+Use the portal to [G57.5,51,1000 Blasted Lands][C] The Blasted Lands 
+Proceed through [G89.5,50,100 Hellfire Peninsula][C] The Dark Portal
+Go to [G56,37,100] Thrallamar [C]
+FP: [F Thrallamar][O][C]
+Complete [QA12388][UI37586-]
+Go to [G27,60,100] Falcon Watch [C]
+FP: [F Falcon Watch][O][C]
+Complete [QA12389][UI37586-]
+Go to [G78.7,63.7,50 Zangarmarsh] Cenarion Refuge[C]
+FP: [F Swamprat Post][O][C]
+Complete [QA12403][UI37586-]
+Go to [G75.5,61,100 Blades Edge Mountains] Mok'Nathal Village [C]
+FP: Go back to [G82,65,50 Zangarmarsh][O][C] Swamprat Post [UI37586-]
+FP:  [F MokNathal Village][O][C]
+Complete [QA12394][UI37586-]
+Go to [G33,64.5,100 Netherstorm] Area 52 [C]
+FP: [F Area 52][O][C]
+Complete [QA12407][UI37586-]
+Go to [G43,46,100 Netherstorm] Stormspire [C]
+FP: [F Stormspire][O][C]
+Complete [QA12408][UI37586-]
+Go to [G62,39,100 Blades Edge Mountains] Evergrove [C]
+FP: [F Evergrove][O][C] \\ To stay in sync with non-FP portion of guide you will pass through Thunderlord, don't worry, you'll be back.
+Complete [QA12406][UI37586-]
+Go to [G53,55.5,100] Thunderlord Village [C]
+FP: [F Thunderlord Village][O][C]
+Complete [QA12393][UI37586-]
+Go to [G32,50,100 Zangarmarsh] Zabra'jin [C]
+FP: [F Zabrajin][O][C]
+Complete [QA12390][UI37586-]
+Go to [G56.5,35.5,100 Nagrand] Garadar [C]
+FP: [F Garadar][O][C]
+Complete [QA12392][UI37586-]
+[A Mage] *Teleport* to [G54,44,200 Shattrath City] Shattrath City [O][C]
+If you're at least unfriendly with the Scryers, I defaulted to Scryer's Tier as it's closer to the FP. If you're Friendly with the Aldor or Hostile with the Scryers you should go to the Aldor Rise.[C]
+Go to [G54,44,100 Shattrath City][C] 
+FP: [F Shattrath][O][C][UI37586-]
+Go to [G27.5,49,10 Shattrath City][A Aldor > 1000][C] Aldor Rise
+Go to [G56,81,10 Shattrath City][A Scryers > -3000][C]  Scryer's Tier
+Complete [QA12404][UI37586-]
+Go to [G49,44,100 Terokkar Forest] Stonebreaker Hold [C]
+FP: [F Stonebreaker Hold][O][C]
+Complete [QA12391][UI37586-]
+Go to [G61,28,100 Shadowmoon Valley] Shadowmoon Village [C]
+FP: [F Shadowmoon Village][O][C]
+Complete [QA12395][UI37586-]
+Same deal as Shattrath, except this time the unaligned are being sent to Altar of Shatar as it's closer.
+FP: [F Altar of ShaTar][A Aldor > -3000][UI37586-][O][C]
+Go to [G61,28,100][A Aldor> -3000][O][C] Altar of ShaTar
+Go to [G56,60,100][A Scryers] Sanctum of the Stars
+FP: [F Sanctum of the Stars][A Scryers > 1000][UI37586-][O][C]
+Complete [QA12409][UI37586-]
+*Congratulations!* You have completed this guide. \\ If you followed these guides in order, you should now have *Tricks and Treats of Azeroth* completed. Otherwise, follow the links below to complete the other parts of this achievement. [UI37586-]
+]], "Gideon Delta Guides")
