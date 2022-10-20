e7d2ac4de515a01a4e26047547537957b7809375
GideonDelta <39777756+GideonDelta@users.noreply.github.com>
2022-10-20 02:58:21 -0400

    Initial upload 0.2.5

---
+++ Guidelime_GideonDelta_WOTLK/Achievements/GD_ToT_EasternKingdoms.lua
@@ -0,0 +1,66 @@
+-- Copyright © 2022 Matthew "GideonDelta" Edmund. All rights reserved.
+-- This add-on is free to download and use. That said, this add-on and its contents may not be redistributed, repackaged, modified, or transferred without explicit and detailed permission of the author.
+-- For questions and feedback childofnewlight@proton.me
+Guidelime.registerGuide(
+[[
+[GA Horde]
+[N Tricks and Treats of Eastern Kingdoms]
+[D Guide designed for efficient completion of the Tricks and Treats of Azeroth achievement for Horde characters:  https://Fyre.is/]
+[NX Tricks and Treats of Kalimdor]
+[NX Tricks and Treats of Outlands]
+Welcome to *GideonDelta's Guides to WOTLK:* Tricks and Treats of Eastern Kingdoms
+This guide is *intended* to be used by characters at the level of the highest zone entered with the apropriate flight paths and riding skill
+My guides focus on efficiency. Whenever possible, I will take you the most direct route with the least amount of time between objectives and the least amount of backtracking.
+Arrive at [G26,73,100 Stranglethorn Vale][C] Booty Bay
+Complete [QA12397][UI37586-]
+Fly to [F Gromgol][C][O]
+Complete [QA12382][UI37586-]
+[A Mage] Mages *teleport* to Stonard[C] 
+Fly to [F Stonard][C]
+[A Mage] Arrive at [G46,53,100 Swamp of Sorrows] Stonard [C]
+Complete [QA12384][UI37586-]
+Fly to [F Kargath][C]
+Complete [QA12385][UI37586-]
+[A Mage] *Teleport* to [G50,16,500 Silvermoon City][UI37586-][O][C] Silvermoon City
+[A Mage] Use *Orb of Translocation* to Ruins of Lordearon, Undercity
+Hearth to [H Dalaran][C] \\ Or Shattrath, Undercity, Silvermoon, or Orgrimmar, otherwise, skip this and continue with the optional steps
+Opt: Fly to [F Gromgol][C][O]
+Opt: Complete [QA12382][UI37586-]
+Opt: Take *Zeppelin* to *Tirisfal Glades* \\[TAR12137] Is the Zeppelin Master[UI37586-][O][C]
+*If you hearthed* this is where you pick up the guide \\ Take portal to [G0,0,1000 The Undercity][O][C] Undercity
+If you hearthed to Undercity, Complete [QA12368][UI37586-] \\ If you missed it or got to Tirisfal another way, we'll come back to this later[O][C]
+[A Mage] Arrive in [G55.3,11,10 Undercity] Ruins of Lordaeron, Undercity[C]
+Go to [G61,58,250 Tirisfal Glades][C] Tirisfal Glades
+Go to [G61.3,51.6,100 Tirisfal Glades][C] Brill
+Complete [QA12363][UI37586-]
+[A Mage] *Teleport* to [G50,16,500 Silvermoon City][UI37586-][O][C] Silvermoon City
+Go to [G66,0,10 Undercity][O][C] Undercity Entrance
+Continue to [G55.3,11,10 Undercity][UI37586-][O][C] *Orb of Translocation*
+Use *Orb of Translocation* to Silvermoon City[O][C]
+Arrive in [G50,16,500 Silvermoon City][UI37586-][O][C] Silvermoon City
+Go to [G79,57.5,30][C] The Royal Exchange
+Complete [QA12369][UI37586-]
+Go to [G67.5,73,30][C] The Bazaar
+Complete [QA12370][UI37586-]
+Exit Silvermoon City and *go west* to [G47,46,100 Eversong Woods][C] Falconwing Square
+Complete [QA12364][UI37586-]
+Go *south* to [G43.5,70.5,100][C] Fairbreeze Village
+Complete [QA12365][UI37586-]
+Go *southeast* to [G47,33,100 Ghostlands][C] Tranquillien
+Complete [QA12373][UI37586-]
+[A Death Knight] Use your *Death Gate* to cutdown flight time to Eastern Plaguelands[C]
+[A Death Knight] Arrive at [G83.7,50,100 Eastern Plaguelands][C] Acherus: The Ebonhold
+Fly to [F Lights Hope Chapel][C]
+Complete [QA12402][UI37586-]
+Fly to [F Revantusk][C]
+Complete [QA12387][UI37586-]
+Fly to [F Hammerfall][C]
+Complete [QA12380][UI37586-]
+Fly to [F Tarren Mill][C]
+Complete [QA12376][UI37586-]
+Fly to [F Sepulcher][C]
+Complete [QA12371][UI37586-]
+Fly to [F Undercity][C] \\ If you plan on doing Outlands, continue to UC as we'll be using the teleporter to Blasted Lands to begin the Outlands guide.
+Complete [QA12368][UI37586-]
+*Congratulations!* You've completed *Tricks and Treats of Eastern Kingdoms* [UI37586-]
+]], "Gideon Delta Guides")
---
+++ Guidelime_GideonDelta_WOTLK/Achievements/GD_ToT_Kalimdor.lua
@@ -0,0 +1,70 @@
+-- Copyright © 2022 Matthew "GideonDelta" Edmund. All rights reserved.
+-- This add-on is free to download and use. That said, this add-on and its contents may not be redistributed, repackaged, modified, or transferred without explicit and detailed permission of the author.
+-- For questions and feedback childofnewlight@proton.me
+Guidelime.registerGuide(
+[[
+[GA Horde]
+[N Tricks and Treats of Kalimdor]
+[D Guide designed for efficient completion of the Tricks and Treats of Azeroth achievement for Horde characters:  https://Fyre.is/]
+[NX Tricks and Treats of Eastern Kingdoms]
+[NX Tricks and Treats of Outlands]
+Welcome to *GideonDelta's Guides to WOTLK:* Tricks and Treats of Kalimdor
+This guide is *intended* to be used by characters at the level of the highest zone entered with the apropriate flight paths and riding skill
+My guides focus on efficiency. Whenever possible, I will take you the most direct route with the least amount of time between objectives and the least amount of backtracking.
+Go to [G53,69,100 Orgrimmar][C] Orgrimmar 
+Complete [QA12366][UI37586-]
+Fly to [F Splintertree Post][C]
+Complete [QA12377][UI37586-]
+[A Druid] Druids, *Teleport* to [G48,38,250 Moonglade][C] Moonglade
+[A Druid] Go to [G32,66.5,10][C] flight path 
+Fly to [F Everlook][C]
+Complete [QA12400][UI37586-]
+[A Mage] Teleport to [G50,50,1000 Orgrimmar][C] Orgrimmar
+Fly to [F Crossroads][C]
+Complete [QA12374][UI37586-]
+Fly to [F Sun Rock Retreat][C]
+Complete [QA12378][UI37586-]
+[A Mage] Teleport to Thunderbluff[C]
+Fly to [F Thunderbluff][C]
+[A Mage] Arrive in [G33,33,500 Thunderbluff][C]
+Complete [QA12367][UI37586-]
+Fly to [F Shadowprey Village][C]
+Complete [QA12381][UI37586-]
+Fly to [F Camp Mojache][C]
+Complete [QA12386][UI37586-]
+Fly to [F Cenarion Hold][C]
+Complete [QA12401][UI37586-]
+Fly to [F Gadgetzan][C]
+Complete [QA12399][UI37586-]
+Fly to [F Freewind Post][C]
+Complete [QA12379][UI37586-]
+Fly to [F Mudsprocket][C]
+Complete [QA12398][UI37586-]
+[F Brackenwall Village][C]
+Complete [QA12383][UI37586-]
+Fly to [F Camp Taurajo][C]
+Complete [QA12375][UI37586-]
+[A Mage] Mages should *Teleport* to [G33,33,500 Thunderbluff][C] Thunderbluff and *Slow Fall* down.
+Go to [G47,60,100 Mulgore][C] Bloodhoof Village
+Complete [QA12362][UI37586-]
+[A Mage] Mages should disregard the next step as they don't need to travel to or wait for lifts. *Teleport* to [G33,33,500 Thunderbluff][C] Thunderbluff
+Go to [G44,59,100 Barrens][C] Camp Taurajo [UI37586-] \\ While TB is technically closer, waiting for a lift, traveling up the lift, and traveling up to the FP make the nearly AFK-able autorun to Camp Taurajo the superior choice
+Fly to [F Ratchet][C]
+Complete [QA12396][UI37586-]
+Go to [G65,34.5,20 Barrens][UI37586-][C] \\ These starting towns are unavoidably hands-on. This route is 50 percent closer than going to Crossroads.
+Cross the river to [G38.5,71,20 Durotar][C]
+Climb the rocks up at [G39.3,71.6][C]
+At [G39.9,71.2][C] jump between the two rocks and you can enter into the Valley of Trials
+Go to [G48.3,67.9,10 Durotar][C] to exit Valley of Trials
+Go to [G52,42,100][C] Razor Hill
+Complete [QA12361][UI37586-]
+*Congratulations!* You've completed *Tricks and Treats of Kalimdor* [UI37586-]
+Follow on to continue to the starting point of the guide for Tricks and Treats of Eastern Kingdoms
+[A Mage] While I didn't test this, it's probably faster to *Teleport* to Orgrimmar, Fly to Ratchet, and skip all but the last step.
+Go to [G50,68.4,10][C] and enter Valley of Trials
+Go to [G41.6,73.3][C] Hidden Path
+Follow path up and to right [G40.7,73,5][C]. If you go straight and off into the water you risk landing on rocks!
+Press into the crevice of the rockface and you can easily walk up without any much or any jumping to [G40.5,72.8][C].
+Carefully make your way back down to the river and cross it to [G65,34.5,20 Barrens]
+Go to [G63.6,38.6,5][C] and take boat to *Booty Bay*
+]], "Gideon Delta Guides")
